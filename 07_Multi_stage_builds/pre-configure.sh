a088c8ce0d3f208334130a6f684a75ca172ba13a
Stefan van Gastel <stefanvangastel@gmail.com>
2018-12-07 14:57:11 +0100

    Tweaks hfdst 7

--- 07_Multi_stage_builds/pre-configure.sh
+++ 07_Multi_stage_builds/pre-configure.sh
@@ -1,5 +1,5 @@
 git clone https://github.com/katacoda/dotnet-aspnet-core-example.git .
 
-rm Dockerfile Dockerfile.multi Makefile docker-compose.yml
+rm Dockerfile Dockerfile.multi Makefile docker-compose.yml .dockerignore
 
 docker rmi -f $(docker images -aq)
