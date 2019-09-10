b27684bdfe8336c9ba63ca98d2615c185a2b1ec2
Stefan van Gastel <stefanvangastel@gmail.com>
2019-09-10 08:38:09 +0200

    Testing

---
+++ git/02_Wijzigingen/courseBase.sh
@@ -0,0 +1,15 @@
+cd myproject
+
+clear
+
+git init
+
+git config --global user.name "Mijn Naam"
+git config --global user.email gebruiker@example.com
+
+clear
+
+git add committed.py
+git commit -m "Eerste wijziging"
+
+clear
