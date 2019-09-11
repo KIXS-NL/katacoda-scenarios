bff3536afe4f259b3fe84402d8eaba0f7675ba61
Stefan van Gastel <stefanvangastel@gmail.com>
2019-09-11 14:25:12 +0200

    3.2 is af

--- git/03_Samenwerken/courseBase.sh
+++ git/03_Samenwerken/courseBase.sh
@@ -6,6 +6,9 @@ clear
 
 git init
 
+git config --global user.name "Pietje Puk"
+git config --global user.email pietje@puk.com
+
 clear
 
 git add .
--- git/03_Samenwerken/createOrigin.sh
+++ git/03_Samenwerken/createOrigin.sh
@@ -1,6 +1,2 @@
 
-git config --global user.name "Pietje Puk"
-git config --global user.email pietje@puk.com
-
-
 git init --bare /remote/myproject.git
