c3641d4891c3ea6a16a9c7e0c6af68e7e75fa082
Stefan van Gastel <stefanvangastel@gmail.com>
2019-09-11 13:50:04 +0200

    Change order

--- git/03_Samenwerken/courseBase.sh
+++ git/03_Samenwerken/courseBase.sh
@@ -6,11 +6,9 @@ clear
 
 git init
 
-git config --global user.name "Pietje Puk"
-git config --global user.email pietje@puk.com
-
 clear
 
+git add .
 git commit -m "Eerste bestand" .
 
 clear
--- git/03_Samenwerken/createOrigin.sh
+++ git/03_Samenwerken/createOrigin.sh
@@ -1,1 +1,6 @@
+
+git config --global user.name "Pietje Puk"
+git config --global user.email pietje@puk.com
+
+
 git init --bare /remote/myproject.git
