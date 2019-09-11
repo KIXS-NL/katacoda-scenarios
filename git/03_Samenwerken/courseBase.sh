6f5d7fc318604aba82e841445cb905026ca51831
Stefan van Gastel <stefanvangastel@gmail.com>
2019-09-11 13:45:59 +0200

    Beginnetje met opdracht 3

---
+++ git/03_Samenwerken/courseBase.sh
@@ -0,0 +1,16 @@
+sleep 1
+
+cd myproject
+
+clear
+
+git init
+
+git config --global user.name "Pietje Puk"
+git config --global user.email pietje@puk.com
+
+clear
+
+git commit -m "Eerste bestand" .
+
+clear
