f1230093321ff641cb6da4dd80d025feb5bf8801
Stefan van Gastel <stefanvangastel@gmail.com>
2019-11-25 15:52:43 +0100

    Fixes in opdracht 8

--- beginner/08_Multi_stage_builds/pre-configure.sh
+++ beginner/08_Multi_stage_builds/pre-configure.sh
@@ -1,5 +1,4 @@
-git clone https://github.com/katacoda/dotnet-aspnet-core-example.git .
-
+git clone https://github.com/katacoda/dotnet-aspnet-core-example.git app
 rm Dockerfile Dockerfile.multi Makefile docker-compose.yml .dockerignore
 
 docker rmi -f $(docker images -aq)
